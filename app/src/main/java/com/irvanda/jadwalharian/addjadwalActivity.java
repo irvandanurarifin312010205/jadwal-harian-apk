53f1a48eec60dd436d15aa92ddf79dbbacab52c6
irvandanurarifin312010205 <irvandanurarifin@gmail.com>
2023-05-13 16:15:33 +0700

    Upload tugas

---
+++ app/src/main/java/com/irvanda/jadwalharian/addjadwalActivity.java
@@ -0,0 +1,14 @@
+package com.irvanda.jadwalharian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addjadwalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addjadwal);
+    }
+}
